0f01bf2f90326cb3b9da02f39e6434e14c439e48
Ankit Raj <ankitraj@Ankits-MacBook-Air.local>
2025-08-16 23:37:52 +0530

    16 Aug

---
+++ Theory1/Theory1.java
@@ -0,0 +1,35 @@
+package Theory1;
+
+// LLD - LLD deals with the Structure of the code.
+// HLD - HLD deals with the Architecture of the Application.
+
+
+
+//  LLD mainly focuses on these 3 aspects : -
+
+/*   1. Scalibility 
+         . Handle large number of users
+         . Code Structure should allow rapid , low - effort expansion
+*/
+
+/*   2. Maintainability 
+         . New Fetures shouldn't break existing ones
+         . Code Should be easy to debug and locate bugs
+*/
+
+/*   3. Reausability 
+         . Write loosely coupled , "plug and play" modules    
+*/
+
+
+
+// What LLD is not ( VS. HLD ) : -
+
+/*   
+         1. HLD deals with the Architecture of the Application
+         2. Tech Stack - Choosing the right tech stack
+         3. Database - SQL vs NoSQL vs Hybrid
+         4. Server Scaling & Deployment - Auto Scaling, Load Balancing, etc.
+         5. Cost Consideration - Minimizing cloud/server costs
+
+*/
---
+++ Theory2/Theory.java
@@ -0,0 +1,10 @@
+package Theory2;
+
+// Pillers of OOPs
+
+/*
+      1. Encapsulation - Bundling data and methods that operate on that data within a single unit or class.
+      2. Abstraction - Hiding complex implementation details and showing only the essential features of the object.
+      3. Inheritance - Mechanism where a new class can inherit properties and behaviors from an existing class.
+      4. Polymorphism - Ability to present the same interface
+ */
