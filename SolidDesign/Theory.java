ca53cf74d34571dc397b472f763bb2bbf8da3d72
Ankit Raj <ankitraj@Ankits-MacBook-Air.local>
2025-08-19 13:37:45 +0530

    18 Aug

---
+++ SolidDesign/Theory.java
@@ -0,0 +1,13 @@
+package SolidDesign;
+
+/*
+     The SOLID principles are a set of five object-oriented design principles introduced by Robert C. Martin (Uncle Bob). 
+     
+      * SOLID Acronym
+      
+        * S → Single Responsibility Principle (SRP)
+        * O → Open/Closed Principle (OCP)
+        * L → Liskov Substitution Principle (LSP)
+        * I → Interface Segregation Principle (ISP)
+        * D → Dependency Inversion Principle (DIP)
+*/
--- UMLDigram/Theory.java
+++ UMLDigram/Theory.java
@@ -7,4 +7,11 @@
   // Thier are 7 types of Structural Diagrams
   // Thier are 7 types of Behavioral Diagrams
 
-  
+  /*
+        * If one class can contain multiple objects of another class in UML, you represent it using multiplicity at the association end:
+
+           . 0..* → zero or many
+           . 1..* → at least one (but possibly many)
+           . 0..1 → zero or one
+           . 1 → exactly one
+   */
